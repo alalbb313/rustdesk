eada74f9a3327773828c0406cd46def41173cc18
alalbb313 <313460137@qq.com>
2026-03-11 00:11:20 +0800

    Fix: always start tray on Windows (including portable mode) so close button hides to tray

--- src/core_main.rs
+++ src/core_main.rs
@@ -83,12 +83,10 @@ pub fn core_main() -> Option<Vec<String>> {
     if args.is_empty() {
         #[cfg(target_os = "linux")]
         let should_check_start_tray = crate::check_process("--server", false);
-        // We can use `crate::check_process("--server", false)` on Windows.
-        // Because `--server` process is the System user's process. We can't get the arguments in `check_process()`.
-        // We can assume that self service running means the server is also running on Windows.
+        // Always start tray on Windows, even in portable mode.
+        // This allows the close button to minimize to tray instead of exiting.
         #[cfg(target_os = "windows")]
-        let should_check_start_tray = crate::platform::is_self_service_running()
-            && crate::platform::is_cur_exe_the_installed();
+        let should_check_start_tray = true;
         if should_check_start_tray && !crate::check_process("--tray", true) {
             #[cfg(target_os = "linux")]
             hbb_common::allow_err!(crate::platform::check_autostart_config());
